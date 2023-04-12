feaaa80968ac4c05446a8320ca7f426a79930c03
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-04-12 15:06:16 -0700

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("file runnning");
 let getAndReturnPeople = async () => {
     let response = await fetch('https://swapi.dev/api/people')
     let parsedResponse = await response.json()
@@ -16,15 +17,49 @@ let peopleArray = getAndReturnPeople();
 
 
 // "Easy" - Loop through this array of objects and and log the name of each person
+// const doProblem = async () => {
+//     let peopleArray = await getAndReturnPeople();
+//     for(let i = 0; i < peopleArray.length; i++) {
+//     console.log(peopleArray[i].name)
+//     }
+// }
+// doProblem()
+// Medium - Loop through this array and make a new array that only contains people that are shorter than 170cm
+// const doProblem = async () => {
+//     let peopleArray = await getAndReturnPeople();
+//     let shortPeopleArray = peopleArray.filter(person => person.height < 170);
+//     console.log(shortPeopleArray);
+//   }
+//   doProblem();              
+// // Hard - loop thorugh and find the tallest and shortest males and females.
 const doProblem = async () => {
     let peopleArray = await getAndReturnPeople();
-    for(let person of peopleArray) {
-    console.log(person.name)
-    }
-}
-// Medium - Loop through this array and make a new array that only contains people that are shorter than 170cm
+  
+    let males = peopleArray.filter(person => person.gender === "male");
+    let females = peopleArray.filter(person => person.gender === "female");
+  
+    let tallestMale = males.reduce((max, person) => max.height > person.height ? max : person);
+    let shortestMale = males.reduce((min, person) => min.height < person.height ? min : person);
+    let tallestFemale = females.reduce((max, person) => max.height > person.height ? max : person);
+    let shortestFemale = females.reduce((min, person) => min.height < person.height ? min : person);
+  
+    console.log(`Tallest male: ${tallestMale.name}`);
+    console.log(`Shortest male: ${shortestMale.name}`);
+    console.log(`Tallest female: ${tallestFemale.name}`);
+    console.log(`Shortest female: ${shortestFemale.name}`);
+  }
+  
+// // Very Hard - loop through and for each person write a string that says "This person is <name> and their ship is <name of ship if any>"
+// // if no ship, say "and they have no ship."
+// const doProblem = async () => {
+//     let peopleArray = await getAndReturnPeople();
+  
+//     for(let person of peopleArray) {
+//       let ship = person.starships.length > 0 ? person.starships[0] : "no ship";
+//       console.log(`This person is ${person.name} and their ship is ${ship}.`);
+//     }
+//   } 
 
-// Hard - loop thorugh and find the tallest and shortest males and females.
-
-// Very Hard - loop through and for each person write a string that says "This person is <name> and their ship is <name of ship if any>"
-// if no ship, say "and they have no ship."
+//: the getAndReturnPeople function returns a promise that resolves with the array of people objects. Therefore, in order to access this array in the rest of the code, we need to either use await or .then() to wait for the promise to resolve. In the provided code, both approaches are demonstrated. The doProblem function uses await to wait for the promise to resolve and then proceeds with the code. The peopleArray variable outside of doProblem uses .then() to wait for the promise to resolve and then logs the resulting array.
+  
+  
